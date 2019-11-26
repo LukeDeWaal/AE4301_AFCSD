45f9ab6ab52e4b61bc88e25e06888438094394a3
Luke de Waal <lr.de.waal.01@gmail.com>
2019-11-26 19:49:51 +0100

    Open Loop Analysis - Start

--- src/open_loop_analysis.m
+++ src/open_loop_analysis.m
@@ -9,8 +9,6 @@ B_long = matrices.B_ac_long;
 C_long = matrices.C_ac_long; 
 D_long = matrices.D_ac_long;
 
-B_long  = [0,0;0.00156899429893435,0.184926492708690;-9.39502701533617e-08,-0.00189996119559329;0,-0.192753630036518];
-
 A_lat = matrices.A_ac_lat; 
 B_lat = matrices.B_ac_lat;
 C_lat = matrices.C_ac_lat; 
@@ -21,5 +19,9 @@ sys_lat = ss(A_lat, B_lat, C_lat, D_lat);
 
 %% Short Period
 t_short = 0:0.001:3;
-u_short = [0 ; 1];
+u_short = [ zeros(1,numel(t_short)) ; 1 zeros(1,numel(t_short)-1)];
+
+[y, x] = lsim(A_long, B_long, C_long, D_long, u_short, t_short);
+plot(t_short, y)
+
 
